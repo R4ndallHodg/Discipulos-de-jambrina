3670c5b73c30459218ec1189b2fab79dd2173b27
DewinU <umana.dewin@gmail.com>
2020-05-30 15:52:33 -0600

    LoginFX Matracazo Full Screen

--- LoginFX/src/controller/loginController.java
+++ LoginFX/src/controller/loginController.java
@@ -1,18 +1,22 @@
 package controller;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import pojo.database;
-
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import pojo.database;
 
 public class loginController implements Initializable {
 
@@ -26,11 +30,18 @@ public class loginController implements Initializable {
         ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
     }
 
-    public void checkLogin() throws SQLException {
+    public void checkLogin() throws SQLException, IOException {
         String username = userTxtField.getText();
         String password = passwordTxtField.getText();
         if(database.is_register(username, password)){
             System.out.println("Correcto");
+            Parent root = FXMLLoader.load(getClass().getResource("../views/mainFrame.fxml"));
+            Scene scene = new Scene(root);
+            Stage  stage = new Stage();
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setMaximized(true);
+            stage.show();
         }
         else{
             System.out.println("Incorrecto");
